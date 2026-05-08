d60a928eacb3c255ebc6cc246f5c48e8207fb32b
zachuary-birdsong1 <zachuary.birdsong@eldonmustangs.org>
2026-05-08 08:04:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,49 +3,31 @@
 # Name: Zachuary Birdsong
 # Date: 5/6/2026
 # Project Title: Final Calculator
-# Description: (Write 1-2 sentences explaining what your program does)
+# Description: (Write 1-2 sentences explaining what your program does)This program will be able to calculate your final grade based on your list of grades given for a single class or give you a final gpa for your grades from your classes.
 # ============================================================
 
+playerName = input("What is your name?")
 
-# ---- SECTION 1: Setup / Variables ----
-# Create your starting variables here.
-# Example: player_name = ""
-
-
-
-# ---- SECTION 2: Welcome Message ----
-# Greet the user and explain what your program does.
-
-print("Welcome!")
+print("Welcome", playerName)
 print("This program will calculate your final grade for a single class or all your classes.")
 
 
-
-# ---- SECTION 3: Get Input from User ----
-# Use input() to ask the user for information.
-# Remember: input() always returns a string.
-# Use int() or float() if you need a number.
-x = input("Do you want to calculate a single class grade or your GPA? Please input either (single class) or (GPA).)
-
-
-
-
-
-# ---- SECTION 4: Logic (if / elif / else) ----
-# Use if/elif/else to make decisions based on user input or variables.
-
-# Example:
-# if score >= 90:
-#     print("Great job!")
-# elif score >= 70:
-#     print("Good work!")
-# else:
-#     print("Keep practicing!")
+option = input("Do you want to calculate a single class grade or your GPA? Please input either 1 for SINGLE CLASS or 2 for GPA: ")
+# This option will end up deciding how they want to calculate there classes.
+if option == "1": 
+    class1 = input("What class are your trying to calculate your final grade for?")
+    userInput = input("Enter grades sperated by spaces: ")
+    grades = [int(x) for x in userInput.split()]
+    average = sum(grades) / len(grades)
+    print("Your grade for class", class1, "is a", float(average), "%!")
+elif option == "2":
+    userInput2 = input("Enter the grade you have for each class, seperated by spaces: ")
+    grades2 = [int(x) for x in userInput2.split()]
+    average2 = sum(grades2) / len(grades2)
+    gpa = (average2 / 100) * 4
+    print("Your final grade", playerName, "is a", float(average2), "%, or a GPA of", float(gpa))
+else:
+    raise ValueError("Invalid statement, please input one of the following: 1 or 2.")
 
 
-
-# ---- SECTION 5: Final Output ----
-# Print a final message, result, or summary to the user.
-
-print("----------------------------")
 print("Thanks for using my program!")
